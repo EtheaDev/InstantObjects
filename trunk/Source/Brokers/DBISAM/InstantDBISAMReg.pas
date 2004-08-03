c98efd17d26fc1d81fbf42d0e46410cbf46ace16
rgrove <rgrove@249abc26-9497-43db-8127-67ec4ea89a2b>
2004-08-03 09:44:25 +0000

    no message

---
+++ trunk/Source/Brokers/DBISAM/InstantDBISAMReg.pas
@@ -0,0 +1,50 @@
+(*
+ *   InstantObjects
+ *   DBISAM Support
+ *)
+
+(* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is: Seleqt InstantObjects
+ *
+ * The Initial Developer of the Original Code is: Seleqt
+ *
+ * Portions created by the Initial Developer are Copyright (C) 2001-2003
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *
+ * ***** END LICENSE BLOCK ***** *)
+
+unit InstantDBISAMReg;
+
+{$IFNDEF VER130}
+{$WARN SYMBOL_PLATFORM OFF}
+{$ENDIF}
+
+interface
+
+procedure Register;
+
+implementation
+
+uses
+  Classes, InstantDBISAM;
+
+procedure Register;
+begin
+  RegisterComponents('InstantObjects', [TInstantDBISAMConnector]);
+end;
+
+end.
