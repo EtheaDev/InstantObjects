eeabb59c1280e0da6e9f38091a4fa820de7b8721
andrea_ap <andrea_ap@249abc26-9497-43db-8127-67ec4ea89a2b>
2004-10-11 08:15:10 +0000

    UIB broker

---
+++ Source/Brokers/UIB/InstantUIBReg.pas
@@ -0,0 +1,42 @@
+(*
+ *   InstantObjects
+ *   UIB Support
+ *)
+
+(* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is: Andrea Petrelli
+ *
+ * The Initial Developer of the Original Code is: Andrea Petrelli
+ *
+ * ***** END LICENSE BLOCK ***** *)
+
+unit InstantUIBReg;
+
+interface
+
+procedure Register;
+
+implementation
+
+uses
+  Classes, InstantUIB, InstantUIBConnection;
+
+procedure Register;
+begin
+  RegisterComponents('InstantObjects', [TInstantUIBConnector]);
+  RegisterComponents('InstantObjects', [TInstantUIBConnection]);
+end;
+
+end.
