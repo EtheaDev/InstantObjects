51653ae525142c666678a52f79d1ac8bf4c348df
Uberto Barbini <uberto.gama@gmail.com>
2005-05-07 08:17:32 +0000

    Simplyfied the tests and added some corner case.

--- Source/Tests/TestInstantCurrency.pas
+++ Source/Tests/TestInstantCurrency.pas
@@ -7,22 +7,15 @@ uses fpcunit, InstantPersistence, InstantMock;
 type
 
   // Test methods for class TInstantCurrency
-  TestTInstantCurrency = class(TTestCase)
+  TTestInstantCurrency = class(TTestCase)
   private
     FAttrMetadata: TInstantAttributeMetadata;
-    FConn: TInstantMockConnector;
     FInstantCurrency: TInstantCurrency;
-    FOwner: TInstantObject;
   public
     procedure SetUp; override;
     procedure TearDown; override;
   published
-    procedure TestAsCurrency;
-    procedure TestAsFloat;
-    procedure TestAsInteger;
     procedure TestAssign;
-    procedure TestAsString;
-    procedure TestAsVariant;
     procedure TestReset;
     procedure TestValue;
   end;
@@ -31,49 +24,22 @@ implementation
 
 uses SysUtils, testregistry, InstantClasses;
 
-procedure TestTInstantCurrency.SetUp;
+procedure TTestInstantCurrency.SetUp;
 begin
-  FConn := TInstantMockConnector.Create(nil);
-  FConn.BrokerClass := TInstantMockBroker;
-  FOwner := TInstantObject.Create(FConn);
   FAttrMetadata := TInstantAttributeMetadata.Create(nil);
   FAttrMetadata.AttributeClass := TInstantCurrency;
   FAttrMetadata.Name := 'AttrMetadataName';
-  FInstantCurrency := TInstantCurrency.Create(FOwner, FAttrMetadata);
+  FInstantCurrency := TInstantCurrency.Create(nil, FAttrMetadata);
   FInstantCurrency.Value := 1.3;
 end;
 
-procedure TestTInstantCurrency.TearDown;
+procedure TTestInstantCurrency.TearDown;
 begin
   FreeAndNil(FInstantCurrency);
   FreeAndNil(FAttrMetadata);
-  FreeAndNil(FOwner);
-  FreeAndNil(FConn);
 end;
 
-procedure TestTInstantCurrency.TestAsCurrency;
-begin
-  FInstantCurrency.AsCurrency := 23.45;
-  AssertEquals('Set AsCurrency is incorrect!', 23.45, FInstantCurrency.Value);
-  AssertEquals('Get AsCurrency is incorrect!', 23.45,
-    FInstantCurrency.AsCurrency);
-end;
-
-procedure TestTInstantCurrency.TestAsFloat;
-begin
-  FInstantCurrency.AsFloat := 89.45;
-  AssertEquals('Set AsFloat is incorrect!', 89.45, FInstantCurrency.Value);
-  AssertEquals('Get AsFloat is incorrect!', 89.45, FInstantCurrency.AsFloat);
-end;
-
-procedure TestTInstantCurrency.TestAsInteger;
-begin
-  FInstantCurrency.AsInteger := 89;
-  AssertEquals('Set AsInteger is incorrect!', 89.0, FInstantCurrency.Value);
-  AssertEquals('Get AsInteger is incorrect!', 89.0, FInstantCurrency.AsInteger);
-end;
-
-procedure TestTInstantCurrency.TestAssign;
+procedure TTestInstantCurrency.TestAssign;
 var
   vSource: TInstantCurrency;
 begin
@@ -88,28 +54,14 @@ begin
   end;
 end;
 
-procedure TestTInstantCurrency.TestAsString;
-begin
-  FInstantCurrency.AsString := '1.3';
-  AssertEquals('Set AsString is incorrect!', 1.3, FInstantCurrency.Value);
-  AssertEquals('Get AsString is incorrect!', '1.3', FInstantCurrency.AsString);
-end;
-
-procedure TestTInstantCurrency.TestAsVariant;
-begin                                           
-  FInstantCurrency.AsVariant := 15.1;
-  AssertEquals('Set AsVariant is incorrect!', 15.1, FInstantCurrency.Value);
-  AssertEquals('Get AsVariant is incorrect!', 15.1, FInstantCurrency.AsVariant);
-end;
-
-procedure TestTInstantCurrency.TestReset;
+procedure TTestInstantCurrency.TestReset;
 begin
   AssertNotNull('Metadata is nil!', FInstantCurrency.Metadata);
   // Metadata.DefaultValue is '';
   FInstantCurrency.Reset;
   AssertEquals('Reset value is incorrect!', 1.3, FInstantCurrency.Value);
 
-  FInstantCurrency.Metadata.DefaultValue := '15.7';
+  FInstantCurrency.Metadata.DefaultValue := '15' + DecimalSeparator + '7';
   FInstantCurrency.Reset;
   AssertEquals('Reset value is incorrect!', 15.7, FInstantCurrency.Value);
 
@@ -119,18 +71,27 @@ begin
   AssertEquals('Reset value is incorrect!', 0.0, FInstantCurrency.Value);
 end;
 
-procedure TestTInstantCurrency.TestValue;
+procedure TTestInstantCurrency.TestValue;
+var
+  c: Currency;
 begin
-  AssertEquals('Value is incorrect!', 1.3, FInstantCurrency.Value);
-  FInstantCurrency.Value := 97.2;
-  AssertEquals('Value is incorrect!', 97.2, FInstantCurrency.Value);
+  c := 123456789012.12345;
+  FInstantCurrency.AsCurrency := c;
+  AssertEquals(c, FInstantCurrency.Value);
+  AssertEquals(c, FInstantCurrency.AsCurrency);
+  c := c + 0.0001;
+  AssertFalse('Precision limit', c = FInstantCurrency.Value);
+  FInstantCurrency.AsCurrency := c;
+  c := c + 0.00001;
+  AssertEquals('Out of precision limit', c, FInstantCurrency.Value);
+  c := -0.0001;
+  FInstantCurrency.AsCurrency := c;
+  AssertEquals(c, FInstantCurrency.Value);
+  AssertFalse(0 = FInstantCurrency.Value);
 end;
 
 initialization
   // Register any test cases with the test runner
-{$IFNDEF CURR_TESTS}
-  RegisterTests([TestTInstantCurrency]);
-{$ENDIF}
+  RegisterTests([TTestInstantCurrency]);
 
 end.
- 
