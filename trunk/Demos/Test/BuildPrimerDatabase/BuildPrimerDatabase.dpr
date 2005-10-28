3a7862a2bf4ae3699547766a0d358c1dab8f4d92
carlobar <carlobar@249abc26-9497-43db-8127-67ec4ea89a2b>
2005-10-28 09:09:26 +0000

    Fixed BuildPrimerDatabase demo

--- trunk/Demos/Test/BuildPrimerDatabase/BuildPrimerDatabase.dpr
+++ trunk/Demos/Test/BuildPrimerDatabase/BuildPrimerDatabase.dpr
@@ -13,7 +13,6 @@ uses
 {$R *.mdr} {Model}
 
 var
-  Connector : TInstantConnector;
   ApplicationPath : string;
   Count : integer;
   ConnectionFile : string;
@@ -27,72 +26,31 @@ begin
     if ParamStr(1) = '' then
       raise Exception.Create('Connection Name Missing');
 
-    Connector := UBuilder.Connect(ConnectionFile, ParamStr(1));
-
-    if Connector = nil then
-      raise Exception.CreateFmt('Connection definition for "%s" not found in %s',[ParamStr(1),ConnectionFile]);
-
-    WriteLn('Building Database structure... please wait.');
-    //Build Database (empty all data)
-    UBuilder.BuildDatabase(Connector);
-
-    //Set default connector
-
-    Connector.IsDefault := True;
-
-
-    WriteLn('Connecting to Database.');
-
-    //Open connection
-
-    Connector.Connect;
-
+    WriteLn('Building Database structure and connect... please wait.');
+    UBuilder.BuildAndConnect(ConnectionFile, ParamStr(1));
 
     if ParamStr(2) <> '' then
-
       Count := StrToInt(ParamStr(2))
-
     else
-
       Count := 100;
 
-
     if SameText(ParamStr(3),'/pictures') then
-
     begin
-
       //Populate Database with Random-Data and Pictures
-
       WriteLn(Format('Building %d Contacs with Pictures... please wait.',[Count]));
-
       UBuilder.CreateRandomContacts(Count, True, ApplicationPath+'..\..\PrimerCross\Pictures\');
-
     end
-
     else
-
     begin
-
       //Populate Database with Random-Data, without Pictures
-
       WriteLn(Format('Building %d Contacs without Pictures... please wait.',[Count]));
-
       UBuilder.CreateRandomContacts(Count);
-
     end;
 
     //Close connection
-
-    Connector.Disconnect;
-
-
+    InstantDefaultConnector.Disconnect;
     WriteLn('Done!');
-
   Except
-
     on E: Exception do WriteLn(E.Message);
-
   End;
-
-
 end.
