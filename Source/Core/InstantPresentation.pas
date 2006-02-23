861a4a9706fb15a518e47a601e9f89ef41083503
jcmoraisjr <jcmoraisjr@249abc26-9497-43db-8127-67ec4ea89a2b>
2006-02-23 18:33:19 +0000

    Rolled back change into GetConnector due to AV introduced into designtime package.

--- Source/Core/InstantPresentation.pas
+++ Source/Core/InstantPresentation.pas
@@ -4504,7 +4504,7 @@ end;
 
 function TInstantSelector.GetConnector: TInstantConnector;
 begin
-  if Assigned(FConnector) or (csDesigning in ComponentState) then
+  if Assigned(FConnector) then
     Result := FConnector
   else
     Result := InstantDefaultConnector;
